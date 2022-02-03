d770959da117b98432083d54a241b92d51cdc651
Martin Pucan <mpucan@trask.cz>
2022-02-03 14:59:05 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from "react";
+import SearchBox from "./components/SearchBox";
+import './components/card-list.styles.css'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [monsters, setMonsters] = useState([])
+    const [searchField, setSearchField] = useState('')
+
+    const handleChange = event => {
+      setSearchField(event.target.value)
+    }
+
+    const filteredMonsters = monsters.filter(monster =>
+         monster.name.toLowerCase().includes(searchField.toLowerCase())
+    )
+
+    useEffect(() => {
+      fetch('https://jsonplaceholder.typicode.com/users')
+          .then(response => response.json())
+          .then(users => {
+              setMonsters(users)
+          })
+    }, [])
+
+    return (
+        <div className="App">
+            <SearchBox placeholder="Search monsters.." handleChange={handleChange} />
+            <ul className="card-list">
+                {
+                    filteredMonsters.map(monster => (
+                        <li key={monster.id} className="card">
+                            <div>{monster.name}</div>
+                            <img
+                                src={`https://robohash.org/${monster.id}?set=set?&size=180x180`}
+                                alt={monster.name}
+                            />
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/CardList.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './card-list.styles.css'
+
+const CardList = () => {
+    return (
+        <ul className="card-list"></ul>
+    )
+}
+
+export default CardList;
